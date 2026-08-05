6f2aee9b70069c87eeee32f98e7c3602c86f77f1
Jir Wong <jirwong@gmail.com>
2026-08-06 08:56:41 +1000

    refactor: make run configuration fields mandatory

--- src/types/configuration.ts
+++ src/types/configuration.ts
@@ -2,9 +2,9 @@ export type RunConfiguration = {
   dbName: string;
   extensions: string[];
   directories: string[];
-  ignore_directories?: string[];
-  update_records?: boolean;
-  process_directories?: boolean;
-  resync_directories?: boolean;
-  resync_check_actual_file?: boolean;
+  ignore_directories: string[];
+  update_records: boolean;
+  process_directories: boolean;
+  resync_directories: boolean;
+  resync_check_actual_file: boolean;
 };
